5edc32b18f59571aae1d4cb62814eefa0d960a67
Saad Abdullah <59808599+saadabdullah1@users.noreply.github.com>
2020-10-03 01:56:02 -0400

    Step 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
-        Button btn = findViewById(R.id.button3); //CHANGE THIS EVERY COMMIT FOR EACH LAYOUT
+        Button btn = findViewById(R.id.button4); //CHANGE THIS EVERY COMMIT FOR EACH LAYOUT
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Switch swt = findViewById(R.id.switch2); //CHANGE THIS EVERY COMMIT FOR EACH LAYOUT
+        Switch swt = findViewById(R.id.switch3); //CHANGE THIS EVERY COMMIT FOR EACH LAYOUT
         swt.setOnClickListener((v) -> swt.setChecked(false));
         swt.setOnCheckedChangeListener((whatClicked, newState) -> {
             if(!newState){
